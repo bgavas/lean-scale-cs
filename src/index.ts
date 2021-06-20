e6ce128fcb587fad5884ae13a3fc753a2ce98ecc
Burak Gavas <burakgavas@Buraks-MacBook-Pro.local>
2021-06-20 19:38:25 +0300

    Add cluster mode

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,30 @@ import dotenv from 'dotenv';
 import 'reflect-metadata';
 dotenv.config();
 import { AppServer } from './server';
+import cluster from 'cluster';
+import os from 'os';
+import { logger } from './utils/logger';
+import { DISABLE_CLUSTER } from './utils/config';
 
-const appServer = new AppServer();
-appServer.setupStatusRoute();
-appServer.startServer();
+const startServer = () => {
+  const appServer = new AppServer();
+  appServer.setupStatusRoute();
+  appServer.startServer();
+};
+
+if (DISABLE_CLUSTER || !cluster.isMaster) {
+  startServer();
+} else {
+  // cluster package already works in round-robin
+  os.cpus().forEach(() => {
+    cluster.fork();
+  });
+
+  cluster.on('online', (worker) => {
+    logger.info(`Worker ${worker.process.pid} started`);
+  });
+
+  cluster.on('exit', (worker) => {
+    logger.info(`Worker ${worker.process.pid} stopped`);
+  });
+}
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,3 @@
 export const ENVIRONMENT = process.env.NODE_ENV;
-
 export const PORT = process.env.PORT;
+export const DISABLE_CLUSTER = process.env.DISABLE_CLUSTER === 'true';
